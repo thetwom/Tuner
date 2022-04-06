a7b4aeb18ae122ebb7191792891d8014227cc62f
thetwom <michael@moekadu.de>
2022-04-06 21:38:37 +0200

    Cents in TargetNote are now always cents.

--- app/src/main/java/de/moekadu/tuner/TargetNote.kt
+++ app/src/main/java/de/moekadu/tuner/TargetNote.kt
@@ -1,5 +1,6 @@
 package de.moekadu.tuner
 
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 class TargetNote {
@@ -37,15 +38,13 @@ class TargetNote {
             recomputeTargetNoteProperties(toneIndex, value, tuningFrequencies)
         }
 
-    /// Current auto-detect frequency range
-    /**
+    /** Current auto-detect frequency range
      * - only when a frequency is outside these bounds, we will search for better fitting tone
      * - to unset a range use a larger value for the first value compared to the second value
      */
     private val frequencyRange = floatArrayOf(1000f, 0f)
 
-    /// Frequency used for last target note detection
-    /**
+    /** Frequency used for last target note detection
      * Negative values unset any previously set frequencies
      */
     private var frequencyForLastTargetNoteDetection = -1f
@@ -56,10 +55,6 @@ class TargetNote {
 
     /// String index of target note
     var stringIndex = -1
-
-//    /// Current note name
-//    var name: CharSequence? = null
-//        private set
 
     /// Frequency of current target note
     var frequency = 0f
@@ -72,16 +67,15 @@ class TargetNote {
     var frequencyLowerTolerance = 0f
         private set
 
-    /// We need some hysteresis effect, before we changing our current tone estimate
-    /**
+    /** We need some hysteresis effect, before we changing our current tone estimate
+     *
      * A value of 0.5f would mean that if we exactly between two half tone, we change our pitch, but
      * this would have no hysteresis effect. So better give a value somewhere between 0.5f and 1.0f
      */
     private val allowedHalfToneDeviationBeforeChangingTarget = 0.6f
 
-    /// Return the tuning status of a
-    // given frequency.
-    /**
+    /** Return the tuning status of a given frequency.
+     *
      * @param currentFrequency Current frequency which should be rated.
      * @return Tuning status of the given frequency.
      */
@@ -92,6 +86,10 @@ class TargetNote {
         else -> TuningStatus.InTune
     }
 
+    /** Set the tone index of the target note explicitly.
+     *
+     * @param toneIndex The tone index which should be set.
+     */
     fun setToneIndexExplicitly(toneIndex: Int) {
         frequencyRange[0] = 100f
         frequencyRange[1] = -100f
@@ -101,6 +99,13 @@ class TargetNote {
         }
     }
 
+    /** Set target note based on a given frequency
+     *
+     * @param frequency The frequency for which we should set the target note
+     * @param ignoreFrequencyRange If this is true, we will set note which is closest to the frequency
+     *   if it is false, we only switch to a new note if we are very clearly closer to another note
+     *   (allowedHalfToneDeviationBeforeTarget).
+     */
     fun setTargetNoteBasedOnFrequency(frequency: Float?, ignoreFrequencyRange: Boolean = false): Int {
         if (ignoreFrequencyRange) {
             frequencyRange[0] = 100f
@@ -161,13 +166,9 @@ class TargetNote {
     /// Recompute current target status.
     private fun recomputeTargetNoteProperties(toneIndex: Int, toleranceInCents: Int, tuningFrequencies: TuningFrequencies) {
         frequency = tuningFrequencies.getNoteFrequency(toneIndex)
-        // TODO: this must change, to have the correct definition of cents in all temperaments
-        //   The cent freq ratio is 2.0.pow(1.0/1200.0)
-        frequencyLowerTolerance = tuningFrequencies.getNoteFrequency(
-            toneIndex - toleranceInCents / 100f)
-        frequencyUpperTolerance = tuningFrequencies.getNoteFrequency(
-            toneIndex + toleranceInCents / 100f)
-//        name = tuningFrequencies.getNoteName(frequency)
+        val toleranceRatio = (2.0.pow(toleranceInCents / 1200.0)).toFloat()
+        frequencyLowerTolerance = frequency / toleranceRatio
+        frequencyUpperTolerance = frequency * toleranceRatio
     }
 
 //    fun getNoteName(context: Context, preferFlat: Boolean): CharSequence {
