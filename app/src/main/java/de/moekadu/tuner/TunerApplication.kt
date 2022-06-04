ae9265e5119df8e2360d03c30499f1c2bb3e0d5d
thetwom <michael@moekadu.de>
2022-06-04 14:11:58 +0200

    Corrections and theme overlay preparations
    - Second text color of note-selector was colorSurfaceVariant, which on some
      cases was hardly readable. This is now changed to colorOnSurfaceVariant.
    - Tests with theme overlays to allow overriding the default background
      color. This is not activated at the moment.

--- app/src/main/java/de/moekadu/tuner/TunerApplication.kt
+++ app/src/main/java/de/moekadu/tuner/TunerApplication.kt
@@ -4,9 +4,9 @@ import android.app.Application
 import com.google.android.material.color.DynamicColors
 
 class TunerApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
-
     }
 }
