287935a09423f7e4b1f1091f0aeb7c580fc09b8d
thetwom <michael@moekadu.de>
2022-12-18 23:22:43 +0100

    Fix frequency detection, when history smoothing is disabled.

--- app/src/main/java/de/moekadu/tuner/notedetection2/TestFunction.kt
+++ app/src/main/java/de/moekadu/tuner/notedetection2/TestFunction.kt
@@ -1,11 +1,10 @@
 package de.moekadu.tuner.notedetection2
 
-import kotlin.math.sin
-
 // no test function
-//    private val testFunction = null
+val testFunction: ((frame: Int, dt: Float) -> Float)? = null
+
 //    // test function, which avoids excessive large times
-//    private val testFunction = { frame: Int, dt: Float ->
+//    val testFunction = { frame: Int, dt: Float ->
 //        val freqApprox = 660f
 //        val numSteps = (1 / freqApprox / dt).roundToInt()
 //        val freq = 1 / (numSteps * dt)
@@ -20,14 +19,14 @@ import kotlin.math.sin
 //    }
 
 // test function with increasing frequency
-val testFunction = { frame: Int, dt: Float ->
-    val freq = 200f + 2f * frame * dt
-    (sin(frame * dt * 2 * kotlin.math.PI.toFloat() * freq)
-            + 0.3f * sin(2 * frame * dt * 2 * kotlin.math.PI.toFloat() * freq)
-            + 0.5f * sin(3 * frame * dt * 2 * kotlin.math.PI.toFloat() * freq))
-}
+//val testFunction = { frame: Int, dt: Float ->
+//    val freq = 200f + 2f * frame * dt
+//    (sin(frame * dt * 2 * kotlin.math.PI.toFloat() * freq)
+//            + 0.3f * sin(2 * frame * dt * 2 * kotlin.math.PI.toFloat() * freq)
+//            + 0.5f * sin(3 * frame * dt * 2 * kotlin.math.PI.toFloat() * freq))
+//}
 
-//    private val testFunction = { t: Float ->
+//    val testFunction = { t: Float ->
 //        val freq = 440f
 //        sin(t * 2 * kotlin.math.PI.toFloat() * freq)
 //        //800f * Random.nextFloat()
