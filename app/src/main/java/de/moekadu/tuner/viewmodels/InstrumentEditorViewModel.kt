79be676de0d05747a0b8d46a44b91afc3a2c34f9
thetwom <michael@moekadu.de>
2023-01-03 22:29:03 +0100

    - Less arguments when creating tuner view model. - Docstrings.

--- app/src/main/java/de/moekadu/tuner/viewmodels/InstrumentEditorViewModel.kt
+++ app/src/main/java/de/moekadu/tuner/viewmodels/InstrumentEditorViewModel.kt
@@ -1,7 +1,6 @@
 package de.moekadu.tuner.viewmodels
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.*
 import de.moekadu.tuner.R
 import de.moekadu.tuner.instruments.Instrument
@@ -22,8 +21,11 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 
+/** Class to store a set of strings and a index of the selected string.
+ * @param strings List of strings.
+ * @param selectedIndex Index of selected string in the strings-list.
+ */
 private class StringsAndSelection(val strings: Array<MusicalNote>, val selectedIndex: Int) {
-
     val note get() = strings.getOrNull(selectedIndex)
 
     fun copy(strings: Array<MusicalNote> = this.strings, selectedIndex: Int = this.selectedIndex): StringsAndSelection {
@@ -31,26 +33,40 @@ private class StringsAndSelection(val strings: Array<MusicalNote>, val selectedI
     }
 }
 
+/** View model for instrument editor.
+ * @param pref App preference resources.
+ */
 class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewModel() {
+    /** Job for detecting notes. This is used to show currently detected notes. */
     private var noteDetectionJob: Job? = null
 
+    /** Sample rate for the frequency detection. */
     val sampleRate = DefaultValues.SAMPLE_RATE
 
+    /** Model for the instrument name. */
     private val _instrumentNameModel = MutableLiveData(InstrumentEditorNameModel("", R.drawable.ic_guitar))
     val instrumentNameModel: LiveData<InstrumentEditorNameModel> get() = _instrumentNameModel
 
+    /** List of strings in the editor. */
     private val _strings = MutableStateFlow(StringsAndSelection(arrayOf(), 0))
     private val strings = _strings.asStateFlow()
 
+    /** Model for the instrument strings. */
     private val _stringViewModel = MutableLiveData(StringViewEditorModel())
     val stringViewModel: LiveData<StringViewEditorModel> get() = _stringViewModel
 
+    /** Model for the note selector. */
     private val _noteSelectorModel = MutableLiveData(NoteSelectorForEditorModel())
     val noteSelectorModel: LiveData<NoteSelectorForEditorModel> get() = _noteSelectorModel
 
+    /** Stable id of the instrument which is modified.
+     * Use Instrument.NO_STABLE_ID, if a new instrument is defined.
+     */
     var stableId = Instrument.NO_STABLE_ID
+    /** Default note, which is used, for new notes if we have no other good choice. */
     private val defaultNote get() = pref.musicalScale.value.referenceNote
 
+    /** Model for showing the lately detected notes. */
     private val _detectedNoteModel = MutableLiveData(DetectedNoteViewModel())
     val detectedNoteModel: LiveData<DetectedNoteViewModel> get() = _detectedNoteModel
 
@@ -121,8 +137,11 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         }}
     }
 
+    /** Get instrument, which is defined in the instrument editor.
+     * @return Instrument of the editor.
+     */
     fun getInstrument(): Instrument {
-        Log.v("Tuner", "InstrumentEditorViewModel: strings = ${strings.value.strings}")
+//        Log.v("Tuner", "InstrumentEditorViewModel: strings = ${strings.value.strings}")
         return Instrument(
             instrumentNameModel.value?.name.toString(), // toString removes underlines and stuff ...
             null,
@@ -132,6 +151,11 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         )
     }
 
+    /** Set the instrument editor to a given instrument.
+     * @param instrument Instrument to set.
+     * @param context Context to obtain string from string resources. If null, the instrument
+     *   name can only be used if it is not based on a string resource.
+     */
     fun setInstrument(instrument: Instrument, context: Context?) {
         _instrumentNameModel.value = InstrumentEditorNameModel(
             instrument.getNameString(context),
@@ -142,10 +166,17 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         stableId = instrument.stableId
     }
 
+    /** Set the editor the the default instrument.
+     * The default instrument is just one string with the reference note.
+     */
     fun setDefaultInstrument() {
         clear(pref.musicalScale.value.referenceNote)
     }
 
+    /** Clear the editor.
+     * @param singleString If this is not null, after clearing we will set one string of the given
+     *   note.
+     */
     fun clear(singleString: MusicalNote? = null) {
         _instrumentNameModel.value = InstrumentEditorNameModel("", R.drawable.ic_guitar)
         _strings.value = StringsAndSelection(
@@ -155,17 +186,26 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         stableId = Instrument.NO_STABLE_ID
     }
 
+    /** Set the instrument name in the editor.
+     * @param name Instrument name.
+     */
     fun setInstrumentName(name: CharSequence?) {
         //Log.v("Tuner", "InstrumentEditorViewModel: Set instrument name: |$name|, current: |${instrumentName.value}|")
         if (name?.contentEquals(instrumentNameModel.value?.name) == false)
             _instrumentNameModel.value = instrumentNameModel.value?.copy(name = name)
     }
 
+    /** Set the instrument icon in the editor.
+     * @param resourceId Resource id of the icon.
+     */
     fun setInstrumentIcon(resourceId: Int) {
         if (resourceId != instrumentNameModel.value?.iconResourceId)
             _instrumentNameModel.value = instrumentNameModel.value?.copy(iconResourceId = resourceId)
     }
 
+    /** Select a string, which is currently modified.
+     * @param stringIndex Index of string to be selected.
+     */
     fun selectString(stringIndex: Int) {
         if (stringIndex != -1) {
             _strings.value = strings.value.copy(selectedIndex = stringIndex)
@@ -184,6 +224,10 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
 //        }
 //    }
 
+    /** Add string below the selected string and select it.
+     * @param string Note of new string to insert. If null we will use the note of the selected
+     *   string or the default note, if no string is selected.
+     */
     fun addStringBelowSelectedAndSelectNewString(string: MusicalNote? = null) {
         val numOldStrings = strings.value.strings.size
         val currentSelectedIndex = strings.value.selectedIndex
@@ -224,6 +268,7 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         }
     }
 
+    /** Delete the currently selected string. */
     fun deleteSelectedString() {
         val stringArray = strings.value.strings
         if (stringArray.isEmpty())
@@ -245,11 +290,17 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         }
     }
 
+    /** Restart sampling for note detection if it is running.
+     * The app preferences will be used to configure the detection.
+     */
     private fun restartSamplingIfRunning() {
         if (noteDetectionJob != null)
             startSampling()
     }
 
+    /** Start sampling for note detection.
+     * The app preferences will be used to configure the detection.
+     */
     fun startSampling() {
         noteDetectionJob?.cancel()
         noteDetectionJob = viewModelScope.launch(Dispatchers.Main) {
@@ -277,16 +328,26 @@ class InstrumentEditorViewModel(private val pref: PreferenceResources) : ViewMod
         }
     }
 
+    /** Stop the note detection job. */
     fun stopSampling() {
         noteDetectionJob?.cancel()
     }
 
+    /** Compute duration in seconds, between two new note detection results.
+     * @param windowSize Number of samples for one frequency evaluation.
+     * @param overlap Overlap between two succeeding windows. (where 0.0 is no overlap;
+     *   1.0 would be full overlap, which is not allowed ...)
+     * @param sampleRate Sample rate.
+     */
     private fun computePitchHistoryUpdateInterval(
         windowSize: Int = pref.windowSize.value,
         overlap: Float = pref.overlap.value,
         sampleRate: Int = this.sampleRate
     ) = windowSize * (1f - overlap) / sampleRate
 
+    /** Factory for creating the view model.
+     * @param pref App preferences.
+     */
     class Factory(private val pref: PreferenceResources) : ViewModelProvider.Factory {
         @Suppress("unchecked_cast")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
