56abefbcab8b207a7d40a39230dc61d26c14ffab
thetwom <michael@moekadu.de>
2024-04-28 20:49:02 +0200

    Minor updates

--- app/src/main/java/de/moekadu/tuner/MainActivity.kt
+++ app/src/main/java/de/moekadu/tuner/MainActivity.kt
@@ -272,7 +272,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
 //        Log.v("Tuner", "MainActivity.onNewIntent: intent=$intent")
         handleFileLoadingIntent(intent)
